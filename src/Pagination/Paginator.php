ac6b5990a768a4983100cd8cba2be52de81e3f5d
阳光锈了 <rusty.sun@qq.com>
2020-06-17 13:05:10 +0800

    fixed:removed Auth,Cookie,Session,View

--- src/Pagination/Paginator.php
+++ src/Pagination/Paginator.php
@@ -5,45 +5,6 @@ namespace Illuminate\Contracts\Pagination;
 interface Paginator
 {
     /**
-     * Get the URL for a given page.
-     *
-     * @param  int  $page
-     * @return string
-     */
-    public function url($page);
-
-    /**
-     * Add a set of query string values to the paginator.
-     *
-     * @param  array|string  $key
-     * @param  string|null  $value
-     * @return $this
-     */
-    public function appends($key, $value = null);
-
-    /**
-     * Get / set the URL fragment to be appended to URLs.
-     *
-     * @param  string|null  $fragment
-     * @return $this|string
-     */
-    public function fragment($fragment = null);
-
-    /**
-     * The URL for the next page, or null.
-     *
-     * @return string|null
-     */
-    public function nextPageUrl();
-
-    /**
-     * Get the URL for the previous page, or null.
-     *
-     * @return string|null
-     */
-    public function previousPageUrl();
-
-    /**
      * Get all of the items being paginated.
      *
      * @return array
@@ -93,13 +54,6 @@ interface Paginator
     public function hasMorePages();
 
     /**
-     * Get the base path for paginator generated URLs.
-     *
-     * @return string|null
-     */
-    public function path();
-
-    /**
      * Determine if the list of items is empty or not.
      *
      * @return bool
@@ -112,13 +66,4 @@ interface Paginator
      * @return bool
      */
     public function isNotEmpty();
-
-    /**
-     * Render the paginator using a given view.
-     *
-     * @param  string|null  $view
-     * @param  array  $data
-     * @return string
-     */
-    public function render($view = null, $data = []);
 }
